d428eb66f272a7587659e8c555bed18b743743f6
luhouxiang <luhouxiang@hotmail.com>
2025-11-17 23:22:24 +0800

    Feat: 恢复信息框的显示

--- klinechart/run/main.py
+++ klinechart/run/main.py
@@ -8,10 +8,12 @@ if "PyQt5" in sys.modules:
 work_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 print("work_dir: ", work_dir)
 sys.path.append(work_dir)
+from common.util import SysLogInit
 
 
 def main():
     """Thin entrypoint: construct QApplication and show main window from gui module."""
+    SysLogInit('kline-chart', 'logs')
     app = QtWidgets.QApplication(sys.argv)
     from klinechart.run.gui import MainWindow
     w = MainWindow()
